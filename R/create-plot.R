b16eb4e24b9dec20e39631033c667794f7f25061
sofia-louise <sofia.pedersen@crick.ac.uk>
2021-02-24 16:10:06 +0000

    Purple

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,17 +15,9 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(col = "green") +
+  geom_point(col = "purple") +
   labs(caption = "Copyright by the Australian Commonwealth Bureau of Meteorology") +
   theme_minimal()
 
 # Save the plot in graphics/
 ggsave(filename = "graphics/scatter-temp-humidity.png", plot = scatter_plot)
-
-time_series =
-  df %>%
-  filter(Location == city) %>%
-  ggplot(aes(x = Date, y = Temp9am)) +
-  geom_line()
-ggsave(filename = "graphics/timeseries-rainfall.png",
-       plot = scatter_plot)
