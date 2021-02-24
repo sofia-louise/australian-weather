bb27452e6b1000a6987a618b55a723b775eaae0e
sofia-louise <79268218+sofia-louise@users.noreply.github.com>
2021-02-24 16:18:08 +0000

    Update create-plot.R

--- R/create-plot.R
+++ R/create-plot.R
@@ -15,7 +15,7 @@ scatter_plot =
 df %>%
   filter(Location == city) %>%
   ggplot(aes(x = Temp9am, y = Humidity9am)) + 
-  geom_point(col = "green") +
+  geom_point(col = "red") +
   labs(caption = "Copyright by the Australian Commonwealth Bureau of Meteorology") +
   theme_minimal()
 
